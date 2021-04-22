95fcfa46f0ed673e98d3d53471818fedb1bc77ad
Rados123 <79587683+Rados123@users.noreply.github.com>
2021-04-22 14:18:17 +0200

    3

--- Trzecie zajęcia/scripts.js
+++ Trzecie zajęcia/scripts.js
@@ -1,15 +1,9 @@
-alert (yo)
+alert ("yo");
 
 const input = document.querySelector('#name');
 input.addEventListener('change', function() {
   const val = this.value;
   const reg = /^[a-zA-ZąĄćĆęĘłŁńŃóÓśŚźŹżŻ]{3,}$/g;
-
-  if (!val.length) {
-    console.log('Pole jest puste')
-  } else {
-    console.log('Twoje imię to: ' + val[0].touppercase() + val.slice(1) );
-  }
 
   if (!reg.test(val)) {
       alert("Imię nie prawidłowe")
@@ -18,4 +12,26 @@ input.addEventListener('change', function() {
       alert('Twoje imię to: ' +val[0].touppercase() + val.slice(1) );
   }
   
+});
+
+
+const input = document.querySelector('#name');
+const val = input.value;
+
+if (!val.length) {
+  console.log('Pole jest puste')
+} else {
+  console.log('Twoje imię to: ' + val[0].touppercase() + val.slice(1) );
+}
+
+const input = document.querySelector('#email');
+email.addEventListener('change', function() {
+  const className = 'errorfield';
+  const mailReg = new RegExp('^[0-9a-z_.-]+@[0-9a-z_.-]+\.[a-z]{2,3}$', 'i');
+
+  if(!mailReg.test(this.value)) {
+    this.classList.add(className)
+  } else {
+    this.classList.remove(className);
+  }
 });
